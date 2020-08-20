c55bbe4c4560dfd77c52336e094dcb4774a9f72b
nfebrian13 <nanaz.febrian@gmail.com>
2020-08-20 20:45:51 +0700

    initial commit

---
+++ social-multiplication-repo/src/main/java/microservices/book/multiplication/SocialMultiplicationRepoApplication.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMultiplicationRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMultiplicationRepoApplication.class, args);
+	}
+
+}
