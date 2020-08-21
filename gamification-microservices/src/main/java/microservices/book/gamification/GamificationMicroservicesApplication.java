1833ea82485d66d20cf0ce0c5ff3f0c876ca5f49
nfebrian13 <nanaz.febrian@gmail.com>
2020-08-21 22:15:09 +0700

    first commit

---
+++ gamification-microservices/src/main/java/microservices/book/gamification/GamificationMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package microservices.book.gamification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamificationMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamificationMicroservicesApplication.class, args);
+	}
+
+}
